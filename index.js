f48d6afdc8460de7a36fa4d6b7735cb951a7a917
Marlon Merjos <marlonmerjos@gmail.com>
2017-09-25 16:43:22 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const app = "I don't do much.";
+
+const token = '3d1fcd989a7447bde8a65faa2db3653cd212bebe'
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
